4172f80556857b4f1cf2266fb03d9f1044d3c8d4
PSSABUNAWAS <pssibnutufailskbt@gmail.com>
2025-10-30 21:58:12 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', e=>{
- e.waitUntil(caches.open('liba-cache').then(c=>c.addAll(['./','./index.html','./app.js','./liba.css'])));
-});
-self.addEventListener('fetch', e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
